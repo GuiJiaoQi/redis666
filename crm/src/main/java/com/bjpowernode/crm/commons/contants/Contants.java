2f433becbc0da0fb2e2c13891868c2fd46865cb2
张凯 <zk@qq.com>
2021-07-21 15:47:42 +0800

    1

--- crm/src/main/java/com/bjpowernode/crm/commons/contants/Contants.java
+++ crm/src/main/java/com/bjpowernode/crm/commons/contants/Contants.java
@@ -5,6 +5,7 @@ package com.bjpowernode.crm.commons.contants;
  * 2021/6/25
  */
 public class Contants {
+    //abc
     public static final String RETURN_OBJECT_CODE_SUCCESS = "1";
     public static final String RETURN_OBJECT_CODE_FAILO = "0";
     public static final String SESSION_USER = "sessionUser";
---
+++ redis/src/main/java/com/bjpowernode/test1.java
@@ -0,0 +1,8 @@
+package com.bjpowernode;
+
+/**
+ * 张凯
+ * 2021/7/21
+ */
+public class test1 {
+}
